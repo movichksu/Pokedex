c16aed2b211e74440e755f36612af86ab9b46840
Kseniya Pakhamovich <kseniya.pakhamovich@innowise-group.com>
2022-05-01 12:00:36 +0300

    feature/make-base-ui - Implement base ui

--- app/src/main/java/com/pahomovichk/pokedex/app/Screens.kt
+++ app/src/main/java/com/pahomovichk/pokedex/app/Screens.kt
@@ -1,5 +1,6 @@
 package com.pahomovichk.pokedex.app
 
+import com.pahomovichk.pokedex.presentation.details.PokemonDetailsFragment
 import com.pahomovichk.pokedex.presentation.pokemonlist.PokemonListFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
@@ -8,5 +9,10 @@ object Screens {
     object PokemonList : SupportAppScreen() {
 
         override fun getFragment() = PokemonListFragment()
+    }
+
+    object PokemonDetails : SupportAppScreen() {
+
+        override fun getFragment() = PokemonDetailsFragment()
     }
 }
