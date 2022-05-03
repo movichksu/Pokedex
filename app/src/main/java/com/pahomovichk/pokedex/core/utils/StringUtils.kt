a2c79019f302d2ed08d278b726041d4e6638c758
Kseniya Pakhamovich <kseniya.pakhamovich@innowise-group.com>
2022-05-03 21:40:07 +0300

    feature/Implement-getting-data-from-pokeapi - Add svg decoders for glide

--- app/src/main/java/com/pahomovichk/pokedex/core/utils/StringUtils.kt
+++ app/src/main/java/com/pahomovichk/pokedex/core/utils/StringUtils.kt
@@ -3,3 +3,6 @@ package com.pahomovichk.pokedex.core.utils
 const val EMPTY_STRING = ""
 fun getPokemonImage(id: String) =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
+
+fun getPokemonArtwork(id: String) =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg"
